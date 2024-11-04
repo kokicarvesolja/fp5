cc7927fabcc45e63d13dde9607a37b1b4eea508d
Kristofer <kristofer.cepon.povsic@gmail.com>
2024-11-04 17:53:59 +0100

    feat: ELO in HOL porocila with bibtex, yay

--- 02Holografija/.auctex-auto/porocilo.el
+++ 02Holografija/.auctex-auto/porocilo.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("graphicx" "") ("longtable" "") ("wrapfig" "") ("rotating" "") ("ulem" "normalem") ("amsmath" "") ("amssymb" "") ("capt-of" "") ("hyperref" "")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("graphicx" "") ("longtable" "") ("wrapfig" "") ("rotating" "") ("ulem" "normalem") ("amsmath" "") ("amssymb" "") ("capt-of" "") ("hyperref" "") ("float" "")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -28,24 +28,24 @@
     "amsmath"
     "amssymb"
     "capt-of"
-    "hyperref")
+    "hyperref"
+    "float")
    (LaTeX-add-labels
     "sec:org072c4ed"
     "eq:1"
     "eq:2"
     "eq:3"
     "eq:4"
-    "fig:1"
+    "slika:1"
     "sec:org2e856b7"
     "eq:5"
     "eq:6"
-    "fig:2"
+    "slika:2"
     "sec:orgc1ffef0"
     "sec:org93ee1e7"
     "sec:org9ab1b67"
     "sec:org1bd5112"
-    "fig:3"
-    "al:1"
+    "slika:3"
     "sec:org51bb9fd"
     "eq:7"))
  :latex)
--- 03Elo/obdelovanje.py
+++ 03Elo/obdelovanje.py
@@ -41,9 +41,10 @@ plt.title("Sorazmernost odziva z modulacijo")
 plt.xlabel(r'$U_s[\mathrm{V}]$')
 plt.ylabel(r'$\psi [\mathrm{V}]$')
 plt.savefig('modulacija.pdf')
+plt.close()
 
 
-# ---- Second part ----
+# --- Second part ---
 
 # funkciiji za fittanje
 #
@@ -64,31 +65,75 @@ y = unp.uarray(data2['Y'], (len(data['Y']) - 1) * 0.01e-3)
 # fittanje podatkov
 
 # poskusil 10, 14, 20, 25
-meje1 = 10
+meje1 = 25
 meje2 = 30
 
 fitpar1, fitcov1 = curve_fit(model1, nu[meje1:], unp.nominal_values(x)[meje1:])
 fitpar2, fitcov2 = curve_fit(model2, nu[meje2:], unp.nominal_values(y)[meje2:]
                              )#, p0=[1, 1], bounds=([0,0], [2, 2]))
 
-print(fitpar2[0], fitpar2[1])
 
 fit1 = model1(nu, *fitpar1)
 fit2 = model2(nu, *fitpar2)
 
-fig, ax = plt.subplots()
 
 # plot for real part
 plt.errorbar(nu, unp.nominal_values(x), yerr=unp.std_devs(x), markersize=2, ls='None',
              marker='o', label=r'$\psi_r$')
 plt.plot(nu, fit1, label='fit real')
 
+# for some reason I had to divide the string into multiple parts
+fittext1 = r'Model fit: $\psi_r$' + '\n' + r'$\psi_0$ = {} ± {}'\
+    .format(format(fitpar1[0], ".4e"), format(fitcov1[0][0]**0.5, ".4e")) + '\n' + r'$\tau$ = {} ± {}'\
+    .format(format(fitpar1[1], ".4e"), format(fitcov1[1][1]**0.5, ".4e"))
+plt.text(0.35, 0.35, fittext1, ha="left", va="center", size=10, transform=ax.transAxes,
+         bbox=dict(facecolor='tab:orange', alpha=0.5))
+
 # plot for imaginary part
 plt.errorbar(nu, unp.nominal_values(y), yerr=unp.std_devs(y), markersize=2, ls='None',
              marker='o', label=r'$\psi_i$')
 plt.plot(nu, fit2, label='fit imaginary')
 
+fittext2 = r'Model fit: $\psi_i$' + '\n' + r'$\psi_0$ = {} ± {}'\
+   .format(format(fitpar2[0], ".4e"), format(fitcov2[0][0]**0.5, ".4e")) + '\n' + r'$\tau$ = {} ± {}'\
+    .format(format(fitpar2[1], ".4e"), format(fitcov2[1][1]**0.5, ".4e"))
+plt.text(0.35, 0.20, fittext2, ha="left", va="center", size=10, transform=ax.transAxes,
+         bbox=dict(facecolor='r', alpha=0.5))
+
 plt.xlabel(r'$\omega [\mathrm{s}^{-1}]$')
 plt.ylabel(r'$\psi [\mathrm{V}]$')
 plt.legend()
-plt.savefig('test.png')
+plt.savefig('fit_modelov.pdf')
+plt.close()
+print(r'$\tau_1$: ', fitpar1[1] * 1e4,  fitcov1[1][1] ** 0.5 * 1e4)
+print(r'$\tau_2$: ', fitpar2[1] * 1e4,  fitcov2[1][1] ** 0.5 * 1e4)
+
+
+# --- Third part ---
+
+# fittanje
+
+
+deljeno = y / x
+
+meja3 = 25
+meja3_k = len(deljeno) - 15
+
+fitpar3, fitcov3 = curve_fit(linear, nu[meja3:meja3_k], unp.nominal_values(deljeno)[meja3:meja3_k])
+
+fit3 = linear(nu[meja3:meja3_k], *fitpar3)
+
+fittext3 = "Linear fit: $y = kn + n$\n$k$ = {} ± {}\n$n$ = {} ± {}".format(format(fitpar3[0], ".4e"), format(fitcov3[0][0]**0.5, ".4e"),
+                                                                           format(fitpar3[1], ".4e"), format(fitcov3[1][1]**0.5, ".4e"))
+plt.text(0.35, 0.45, fittext3, ha="left", va="center", size=10, transform=ax.transAxes,
+         bbox=dict(facecolor='b', alpha=0.5))
+
+plt.errorbar(nu[meja3:meja3_k], unp.nominal_values(deljeno)[meja3:meja3_k], yerr=unp.std_devs(deljeno)[meja3:meja3_k], markersize=2, ls='None', marker='o', label=r'$\frac{\psi_i}{\psi_r}$')
+plt.plot(nu[meja3:meja3_k], fit3, label='fit')
+
+plt.title('Razmerje med signaloma')
+plt.xlabel(r'$\omega [\mathrm{s} ^{-1}]$')
+plt.ylabel(r'$\frac{\psi_i}{\psi_r}$')
+plt.legend()
+plt.savefig('deljena.pdf')
+print(r'$\tau_3$: ',  fitpar3[0] * 1e4, fitcov3[0][0] ** 0.5 * 1e4)
