722ce9823a3b871b267177b4c0ec209f2cb587a4
Kristofer <kristofer.cepon.povsic@gmail.com>
2024-11-17 20:39:25 +0100

    feat: graphs have vertical lines and ubranost je pravilna, wtf is my life

--- 04mikroval/source/obdelovanje.py
+++ 04mikroval/source/obdelovanje.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import cmasher as cmr
+from uncertainties import unumpy as unp
 
-# v datoteki csv.txt imaš podatke o začetni poziciji ch1 in ch2. Odštej ju od ch1 in ch2, ki sta extracted
-# prav tako naredi plt.twinx pri bolometru in kratkostični steni
+# calculate \beta x and other values, put them into report and let the freaking
+# magic happen.
 
 # defining class for easier handling
 class Oscilo:
@@ -17,6 +18,8 @@ class Oscilo:
     #extract time, might need to add time scale as an argument
     def time(self):
         return self.data[:, 0] * 5e-2
+    def length(self):
+        return np.linspace(0, 6.2, num=1200)
     #extract channel 1 and normalize it (sonda)
     def ch1(self):
         return (self.data[:, 1] - np.min(self.data[:, 1])) / (np.max(self.data[:, 1]) - np.min(self.data[:, 1]))
@@ -54,7 +57,7 @@ c4, c5 = cmr.take_cmap_colors("cmr.bubblegum", 2, cmap_range=(0.25, 0.75), retur
 #
 kratko = Oscilo(pd.read_csv(r'../meritve/kiki_kratki.csv'), 5.88, 0.087)
 fig, ax1 = plt.subplots(2, 1)
-ax1[0].scatter(kratko.time(), kratko.ch1(), c=c5, marker='.', label='sonda', zorder=10)
+ax1[0].scatter(kratko.time(), kratko.ch2(), c=c4, marker='.', label='meritve kratkostične stene', zorder=1)
 ax1[0].set_xlabel(r'$t[\mathrm{ms}]$')
 ax1[0].set_ylabel(r'signal $[\mathrm{V}]$')
 ax1[0].set_title('Kratkostična stena')
@@ -62,7 +65,7 @@ ax1[0].set_title('Kratkostična stena')
 # creating a twing axes for kratkostična stena
 
 ax2 = ax1[0].twinx()
-ax2.scatter(kratko.time(), kratko.ch2(), c=c4, marker='.', label='meritve kratkostične stene')
+ax2.scatter(kratko.time(), kratko.ch1(), c=c5, marker='.', label='sonda', zorder=2)
 ax2.set_ylabel(r'potenciometer $[0, 1]$')
 
 
@@ -82,35 +85,48 @@ ax3.set_ylabel(r'potenciometer $[0, 1]$')
 fig.tight_layout()
 fig.savefig('../figures/kratkoInBolo')
 plt.close()
-#plt.scatter(kratko.time(), kratko.ch1(), c=c4, marker='.',label='sonda')
-#plt.scatter(kratko.time(), kratko.ch2_norm(), c=c5, marker='.',label='meritve')
-#plt.title('Meritve s kratkostično steno')
-#plt.legend()
-#plt.savefig('../figures/test.png')
-#plt.close()
 
 antena = Oscilo(pd.read_csv(r'../meritve/kik_antena.csv'), 5.88, 0.035)
-#plt.plot(antena.time(), antena.ch1(), c=c4, marker='.',label='sonda')
-#plt.plot(antena.time(), antena.ch2_norm(), c=c5, marker='.',label='meritve')
-#plt.title('Meritve z anteno')
-#plt.legend()
-#plt.savefig('../figures/test_antena.png')
-#plt.close()
-
-#plt.plot(bolometer.time(), bolometer.ch1(), c=c4, marker='.',label='sonda')
-#plt.plot(bolometer.time(), bolometer.ch2_norm(), c=c5, marker='.',label='meritve')
-#plt.title('Meritve z bolometrom')
-#plt.legend()
-#plt.savefig('../figures/test_bolometer.png')
-#plt.close()
-
-# krivulja ubranosti
-#plt.scatter(kratko.time(), kratko.ch2(), c=c4, marker='.', label='kratko')
-#plt.scatter(bolometer.time(), bolometer.ch2(), c=c5, marker='.', label='bolometer')
-#
-#plt.legend()
-#plt.savefig('../figures/kratkoInBolo.png')
-#plt.close()
 
 
-# data might be wrong
+# --- ubranost ---
+# shortening of arrays (using signal on interval 2.0 cm to 6.2 cm)
+short_length = bolometer.length()[2.0 <= bolometer.length()]
+short_bolometer = bolometer.ch2()[2.0 <= bolometer.length()]
+short_kratko = kratko.ch2()[2.0 <= bolometer.length()]
+
+# izračun
+# minimum bolometra, napaka je \pm na zadnjo decimalko
+
+minimum = unp.uarray(np.min(short_bolometer), 0.001)
+maksimum = unp.uarray(np.max(short_bolometer), 0.001)
+
+indeks_minimuma = np.argmin(short_bolometer)
+indeks_maksimuma = np.argmax(short_bolometer)
+
+# drawing data
+
+plt.scatter(short_length, short_bolometer, marker='.', c=c4, label='bolometer')
+plt.scatter(short_length, short_kratko, marker='.', c=c5,
+            label='kratkostična stena')
+
+# drawing horizontal lines where I took the minimum/maximum value
+
+plt.hlines(short_bolometer[indeks_minimuma], 2.0, 6.21, color=c3)
+plt.hlines(short_bolometer[indeks_maksimuma], 2.0, 6.21, color=c3)
+
+# drawing vertical lines for difference between two minimums of bolometer and
+# kratkostična stena
+
+# everything else for beautiful graphs
+plt.xlabel(r'Razdalja $[\mathrm{cm}]$')
+plt.ylabel(r'Signal $[\mathrm{V}]$')
+plt.legend()
+plt.title('Krivulja ubranosti')
+plt.savefig('../figures/krivulja_ubranost.png')
+plt.close()
+
+# ubranost
+
+s = unp.sqrt(maksimum / minimum)
+print(s)
