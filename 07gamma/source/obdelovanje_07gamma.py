7be7b11d86b1f3a2b36c36ff2d25dbe16337e685
Kristofer <kristofer.cepon.povsic@gmail.com>
2024-12-03 15:31:53 +0100

    feat: worked on gamma analysis

--- 07gamma/source/obdelovanje_07gamma.py
+++ 07gamma/source/obdelovanje_07gamma.py
@@ -41,10 +41,82 @@ plt.close()
 
 #--- večkanalne meritve ---
 
+# kalibracija
+
+def linearna(x, k, n):
+    return k * x + n
+
+def gauss(x, mi, sigma, C):
+    return (C / sigma * np.sqrt(2 * np.pi)) *\
+        np.exp(- (1 / 2) * (x - mi) ** 2 / (sigma ** 2))
+
+def gauss_indeks(array, start_indeks, end_indeks):
+    indeks = np.arange(len(array))
+    fitpar_g, fitcov_g = curve_fit(gauss, indeks[a:b], array[a:b])
+    return fitpar_g
+
+def backscatter(energy):
+    return energy/(1 + 2 * energy/0.51)
+
+def comptonpeak(energy):
+    return energy * 2 * (energy/0.51) ( 1 + 2 * energy / 0.51 )
+
+# more colors
+
+d1, d2, d3 = cmr.take_cmap_colors('cmr.cosmic', 3, cmap_range=(.3, .8),
+                              return_fmt='hex')
+
+# kalibracija z natrijem
+
+calna_cas = 157.8
+data_cal = np.loadtxt('../meritve/nacal.txt')
+
+# indeksi znanih maksimumov
+maks1, argmax = np.max(data_cal), np.argmax(data_cal)
+#print('arg max: ', argmax)
+#print('maks1: ', maks1)
+
+maks2, argmax2 = np.max(data_cal[1800:]), np.argmax(data_cal[1800:])
+#print('arg max: ', argmax2)
+#print('maks1: ', maks2)
+
+# indeksa vrhov 0.51 in 1.277 MeV sta pri indeksih
+prvi_max = 1493
+drugi_max = 1800 + 1825
+fitpar, fitcov = curve_fit(linearna, [prvi_max, drugi_max],\
+                           [0.51, 1.277])
+# predpostavljam, da je kalibracija linearna, which according to
+# Marko's Chest ni. Ampak se mi ne da ponovno delati stvari.
+x_cal = linearna(np.arange(0, len(data_cal)), *fitpar)
+
+plt.bar(x_cal, data_cal/calna_cas, width=0.001, color=d1)
+plt.vlines(0.51, 0, 2.0, color=d2, ls = '--')
+plt.vlines(1.277, 0, 2.0, color=d2, ls = '--')
+plt.title(r'Spekter $^{22} \mathrm{Na}$')
+plt.xlabel(r'$E [\mathrm{MeV}]$')
+plt.ylabel(r'$R [\mathrm{s} ^{-1}]$')
+plt.savefig('../porocilo/figures/kalibracija.png')
+
+print('done calibrating')
+
 # šum ozadja
 
-data_bg = np.loadtxt('../meriteve/bg.txt')[85:] # real data doesn't start until line 85
+data_bg = np.loadtxt('../meritve/bg.txt') # real data doesn't start until line 85
 time_bg = 657
 
+plt.bar(x_cal, data_bg/time_bg, width=0.001, color=d1)
+plt.title('Šum ozadja')
+plt.xlabel(r'$E [\mathrm{MeV}]$')
+plt.ylabel(r'$R [\mathrm{s} ^{-1}]$')
+plt.savefig('../porocilo/figures/ozadje.png')
+
+print('done background noise')
+
+# natrij brez šuma
+
+
+
 # aktivnost označena z R
+
+
 r_bg = data_bg / time_bg
