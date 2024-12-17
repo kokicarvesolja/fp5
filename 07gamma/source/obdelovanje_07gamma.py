941c70ffad76bb01824f60d15d28dd250a5d6cb2
Kristofer <kristofer.cepon.povsic@gmail.com>
2024-12-17 09:48:50 +0100

    new: intro to diffusion

--- 07gamma/source/obdelovanje_07gamma.py
+++ 07gamma/source/obdelovanje_07gamma.py
@@ -50,12 +50,11 @@ def linearna(x, k, n):
 def gauss(x, C, mi, sigma):
     return C * np.exp(- (x - mi) ** 2 / (2 * sigma ** 2))
 
-
 def backscatter(energy):
     return energy/(1 + 2 * energy/0.51)
 
 def comptonpeak(energy):
-    return energy * 2 * (energy/0.51) ( 1 + 2 * energy / 0.51 )
+    return energy * 2 * (energy/0.51) / ( 1 + 2 * energy / 0.51 )
 
 # more colors
 
@@ -130,10 +129,23 @@ start_peak1 = 1493 - 150
 end_peak1 = 1493 + 150
 
 # omejim x_cal za lažji plot
-xcal_Na22_p1 = x_cal[start_peak1: end_peak1]
+xcal_Na22_p1 = x_cal[start_peak1:end_peak1]
 
-fitpar_na22_p1, fitcov_na22_p2 = curve_fit(gauss, xcal_Na22_p1,
+fitpar_na22_p1, fitcov_na22_p1 = curve_fit(gauss, xcal_Na22_p1,
                                            r_na22[start_peak1:end_peak1], p0=[1,1,1])
+
+sigma_Na1 = unp.uarray(fitpar_na22_p1[2], np.sqrt(np.diag(fitcov_na22_p1))[2])
+# širina vrhov je sigma
+print('Širina prvega vrha za natrij: ', sigma_Na1)
+
+# calculating energijsko ločljivost according to
+
+E0_Na1 = unp.uarray(fitpar_na22_p1[1], np.sqrt(np.diag(fitcov_na22_p1))[1])
+
+# 2.355 * FWHM je enako sigma Gaussove funkcije
+print("R natrija: ", 2.355 * sigma_Na1/E0_Na1)
+
+# računanje preko np.sum(r_na22) ali pa preko np.sqrt(np.sum(r_na22)) ne dela
 
 plt.plot(xcal_Na22_p1, gauss(xcal_Na22_p1, *fitpar_na22_p1), color=e2)
 
@@ -145,15 +157,216 @@ xcal_Na22_p2 = x_cal[start_peak2:end_peak2]
 
 fitpar_na22_p2, fitcov_na22_p2 = curve_fit(gauss, xcal_Na22_p2,
                                            r_na22[start_peak2:end_peak2])
+
+sigma_Na2 = unp.uarray(fitpar_na22_p2[2], np.sqrt(np.diag(fitcov_na22_p2))[2])
+print('Širina drugega vrhu za natrij: ', sigma_Na2)
+
+# Energijska ločljivost drugega vrha natrija
+E0_Na2 = unp.uarray(fitpar_na22_p2[1], np.sqrt(np.diag(fitcov_na22_p2))[1])
+
+print("R drugega vrha natrija: ", 2.355 * sigma_Na2 / E0_Na2)
+
 plt.plot(xcal_Na22_p2, gauss(xcal_Na22_p2, *fitpar_na22_p2), color=e3)
 
+# backscatter and Compton peak, v enačbo vstaviš energijo prvega (ali edinega) vrha
+
+backscatterNa22 = backscatter(0.511)
+comptonPeakNa22 = comptonpeak(0.511)
+
+print("Backscatter peak natrija: ", backscatterNa22)
+print("Compton peak natrija: ", comptonPeakNa22)
+
+plt.vlines(backscatterNa22, 0, 2.0, color=d3, ls='dotted',
+           label="predvideno povratno sipanje")
+plt.vlines(comptonPeakNa22, 0, 2.0, color=e1, ls='dotted',
+           label='predviden Comptonski vrh')
+
+# plot of bars and energy
+
 plt.bar(x_cal, r_na22, width=0.001, color=d1)
-plt.vlines(0.51, 0, 2.0, color=d2, ls = '--', zorder=1)
-plt.vlines(1.277, 0, 2.0, color=d2, ls = '--', zorder=1)
+plt.vlines(0.51, 0, 2.0, color=d2, ls = '--', zorder=1,
+           label=r'$E_1 = 0.51 \mathrm{MeV}$')
+plt.vlines(1.277, 0, 2.0, color=d2, ls = '--', zorder=1,
+           label=r'$E_2 = 1.27 \mathrm{MeV}$')
 plt.title(r'Spekter $^{22} \mathrm{Na}$ brez ozadja')
 plt.xlabel(r'$E [\mathrm{MeV}]$')
 plt.ylabel(r'$R [\mathrm{s} ^{-1}]$')
+plt.legend()
 plt.savefig('../porocilo/figures/na22_no_bg.png')
 plt.close()
 
 print('Done plot of natrij without background')
+
+# --- cezij brez šuma ---
+
+dataCs = np.loadtxt('../meritve/Cs137_1.txt')
+timeCs = 59.92
+
+# aktivnost cezija
+
+r_cs137 = dataCs / timeCs - r_bg
+
+# getting the index of Cs
+
+argmaxCs = np.argmax(r_cs137[150:]) + 150
+print("Vrh cezija: ", x_cal[argmaxCs])
+
+startPeakCs = argmaxCs - 200
+endPeakCs = argmaxCs + 200
+
+# adjusting x axis
+xCalCs137 = x_cal[startPeakCs:endPeakCs]
+
+fitparCs137, fitcovCs137 = curve_fit(gauss, xCalCs137,
+                                     r_cs137[startPeakCs:endPeakCs])
+
+sigmaCs =  unp.uarray(fitparCs137[2], np.sqrt(np.diag(fitcovCs137))[2])
+E0Cs = unp.uarray(fitparCs137[1], np.sqrt(np.diag(fitcovCs137))[2])
+
+# širina vrha
+
+print("Širina prvega vrha cezija: ", sigmaCs)
+print("R cezija: ", 2.355 * sigmaCs / E0Cs)
+
+# backscatter and Compton peak
+
+backscatterCs = backscatter(E0Cs)
+comptonPeakCs = comptonpeak(E0Cs)
+
+print("Backscatter peak Cs: ", backscatterCs)
+print("Compton peak Cs: ", comptonPeakCs)
+
+plt.vlines(unp.nominal_values(backscatterCs), 0, 3.5, color=d3, ls='dotted',
+           label="predvideno povratno sipanje")
+plt.vlines(unp.nominal_values(comptonPeakCs), 0, 3.5, color=e1, ls='dotted',
+           label='predviden Comptonski vrh')
+
+# plot of Gauss function
+
+plt.plot(xCalCs137, gauss(xCalCs137, *fitparCs137), color=e3)
+
+# bar plot of cezij
+plt.bar(x_cal, r_cs137, width=0.001, color=d1)
+
+# the maximum of cezij
+plt.vlines(x_cal[argmaxCs], 0, 3.5, color=d2, ls = '--', zorder=1,
+           label=r'$E_1 = (0.66 \pm 0.04) \mathrm{MeV}$')
+
+# miscs for matplotlib
+plt.legend()
+plt.title(r'Spekter $^{137} \mathrm{Cs}$ brez ozadja')
+plt.xlabel(r'$E [\mathrm{MeV}]$')
+plt.ylabel(r'$R [\mathrm{s} ^{-1}]$')
+plt.savefig('../porocilo/figures/Cs137_no_bg.png')
+plt.close()
+
+print("Finished plot of Cs")
+
+# --- kobalt brez šuma ---
+
+print("Plot of Co")
+dataCo = np.loadtxt('../meritve/Co60.txt')
+timeCo = 54.8
+
+# aktivnost kobalta
+
+r_Co60 = dataCo / timeCo - r_bg
+
+# getting first index of Co
+
+argmaxCo1 = np.argmax(r_Co60)
+print("Vrh 1 od kobalta: ", x_cal[argmaxCo1])
+
+# getting second index of Co
+
+argmaxCo2 = np.argmax(r_Co60[(argmaxCo1 + 100):]) + (argmaxCo1 + 100)
+print("Vrh 2 od Co: ", x_cal[argmaxCo2])
+
+# fitting for the first peak of Co
+
+startPeakCo1 = argmaxCo1 - 300
+endPeakCo1 = argmaxCo1 + 300
+
+# adjusting x axis
+
+xCalCo1 = x_cal[startPeakCo1:endPeakCo1]
+
+fitparCo1, fitcovCo1 = curve_fit(gauss, xCalCo1,
+                                 r_Co60[startPeakCo1:endPeakCo1])
+
+# energijska ločljivost prvega vrha
+sigmaCo1 =  unp.uarray(fitparCo1[2], np.sqrt(np.diag(fitcovCo1))[2])
+E0Co1 = unp.uarray(fitparCo1[1], np.sqrt(np.diag(fitcovCo1))[2])
+
+# širina prvega vrha kobalta
+
+print("Širina prvega vrha kobalt: ", sigmaCo1)
+print('R prvega vrha Co: ', 2.355 * sigmaCo1 / E0Co1)
+
+plt.plot(xCalCo1, gauss(xCalCo1, *fitparCo1), color=e2)
+
+# backscatter and compton peak of Co
+backscatterCo = backscatter(E0Co1)
+comptonPeakCo = comptonpeak(E0Co1)
+
+print("Backscatter peak kobalta: ", backscatterCo)
+print("Compton peak kobalta: ", comptonPeakCo)
+
+
+plt.vlines(unp.nominal_values(backscatterCo), 0, 1.5, color=d3, ls='dotted',
+           label="predvideno povratno sipanje")
+plt.vlines(unp.nominal_values(comptonPeakCo), 0, 1.5, color=e1, ls='dotted',
+           label='predviden Comptonski vrh')
+
+# fitting for the second peak of Co
+
+startPeakCo2 = argmaxCo2 - 200
+endPeakCo2 = argmaxCo2 + 200
+
+xCalCo2 = x_cal[startPeakCo2:endPeakCo2]
+
+fitparCo2, fitcovCo2 = curve_fit(gauss, xCalCo2,
+                                 r_Co60[startPeakCo2:endPeakCo2])
+
+# energijska ločljivost drugega vrha kobalta
+sigmaCo2 =  unp.uarray(np.abs(fitparCo2[2]), np.sqrt(np.diag(fitcovCo2))[2])
+E0Co2 = unp.uarray(fitparCo2[1], np.sqrt(np.diag(fitcovCo2))[2])
+
+# širina prvega vrha kobalta
+
+print("Širina drugega vrha kobalta: ", sigmaCo2)
+print("R drugega vrha Co: ", 2.355 * sigmaCo2 / E0Co2)
+
+plt.plot(xCalCo2, gauss(xCalCo2, *fitparCo2), color=e3)
+
+plt.bar(x_cal, r_Co60, width=0.001, color=d1)
+
+# first peak
+plt.vlines(x_cal[argmaxCo1], 0, 1.5, color=d2, ls='--', zorder=1,
+           label=r'$E_1 = (1.16 \pm 0.1) \mathrm{eV}$')
+
+# second peak
+
+plt.vlines(x_cal[argmaxCo2], 0, 1.5, color=d2, ls='--', zorder=1,
+           label=r'$E_2 = (1.33 \pm 0.1) \mathrm{eV}$')
+
+# miscs for matplotlib
+plt.legend()
+plt.title(r'Spekter $^{60} \mathrm{Co}$ brez ozadja')
+plt.xlabel(r'$E [\mathrm{MeV}]$')
+plt.ylabel(r'$R [\mathrm{s} ^{-1}]$')
+plt.savefig('../porocilo/figures/Co60_no_bg.png')
+plt.close()
+
+# --- izkoristek ---
+
+aktivnost = unp.uarray(np.sum(r_cs137), 2)
+A0 = 9250 # podatki iz leta 2013
+tau = 30.07 # in years
+trenutno = 2024 - 2013
+
+izracunanaAktivnost = A0 * np.exp( - trenutno / tau)
+print(izracunanaAktivnost)
+
+effective = aktivnost / izracunanaAktivnost
+print('\eta = ', effective)
