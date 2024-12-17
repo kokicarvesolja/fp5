be5b4e9d27a6eedc030955e0fcc0c4eef5727a32
Kristofer <kristofer.cepon.povsic@gmail.com>
2024-12-17 15:02:11 +0100

    fix: 07Gamma and feat:working on radioactive decay

--- 08gammagamma/source/obdelovanja_08gg.py
+++ 08gammagamma/source/obdelovanja_08gg.py
@@ -13,6 +13,11 @@ import pandas as pd
 c1, c2, c3 =  cmr.take_cmap_colors('cmr.cosmic', 3, cmap_range=(.5, 1.),
                                    return_fmt='hex')
 
+# functions needed
+
+def linear(x, k, n):
+    return k * x + n
+
 # --- časovna ločljivost ---
 
 dataCasLoc = np.loadtxt('../meritve/casovna_locljivost_data.txt')
@@ -27,3 +32,23 @@ plt.ylabel(r'$N$')
 plt.title('Časovna ločljivost TDC')
 plt.savefig('../porocilo/figures/casovna_locljivost.png')
 plt.close()
+
+# --- radioaktivni razpad ---
+
+dataRadRaz = np.loadtxt('../meritve/radioaktivni_razpad_data.txt')
+casMerjenjaRadRaz = 31.6
+
+# Graf radioaktivnih razpadov
+
+plt.bar(dataRadRaz[:, 0], dataRadRaz[:, 1] / casMerjenjaRadRaz, width=0.005, color=c1)
+
+plt.xlabel(r'$t [\mathrm{ns}]$')
+plt.ylabel(r'$R [\mathrm{ns}^{-1}]$')
+plt.title(r'Radioaktivni razpad $^{22} \mathrm{Na}$ ')
+plt.savefig('../porocilo/figures/radio_razpad.png')
+plt.close()
+
+# Logaritmiran graf radioaktivnih razpadov
+
+casRadRaz = dataRadRaz[:, 0]
+meritveRadRaz = dataRadRaz[:, 1]
