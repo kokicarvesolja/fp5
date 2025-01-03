30fbe242b54bbd5bd2acfcfd9fdf404e6d1ef2fe
Kristofer <kristofer.cepon.povsic@gmail.com>
2025-01-03 17:21:00 +0100

    11 analysis: working on diffusion constant graph

--- 11difuzija/source/obdelovanje_11dif.py
+++ 11difuzija/source/obdelovanje_11dif.py
@@ -6,17 +6,26 @@ from scipy.optimize import curve_fit
 import cmasher as cmr
 from uncertainties import unumpy as unp
 from uncertainties import ufloat
+import pandas as pd
 
 # izmerjeni podatki
 a = ufloat(41, 0.5) # razdalja leca - kiveta
 b = ufloat(110, 0.5) # razdalja kiveta - zaslon
 d = ufloat(1.8, 0.1) # debelina kivete
 
+# colours
 
+c1, c2, c3 = cmr.take_cmap_colors("cmr.gem", 3, cmap_range=(0.2, 1),
+                                  return_fmt="hex")
 # --- casovna neodvisnost ploscine S ---
 # za ploscino sem uporabil web plot digitizer (https://apps.automeris.io/wpd4/)
+# File > Load Image. Calibrate by choosing two (x, y) points, than under section
+# measurements choose Area and add polygons. Area is given in pixels, so measure
+# how much is 1cm^2 in pixels.
 
-ploscina = ((a + b) / a) * d * 0.029 # razlika n_a - n_v = 0.029, podano v navodilih
+k = (a + b) / a
+
+ploscina = k * d * 0.029 # razlika n_a - n_v = 0.029, podano v navodilih
 print('ploscina S tocno: ', ploscina)
 
 # za case t=0, 33.5, 70
@@ -31,3 +40,15 @@ povrsina3kotnika = 1362893 - 242335
 povrsina -= povrsina3kotnika
 
 print('ploscina S glede na y_max: ', (povrsina/ kvadratniCm))
+
+# --- dolocanje difuzijske konstante ---
+
+# risemo graf 1 / (4 * np.pi * k ** 2) * (ploscina / visina) ** 2 v odvisnosti od
+# casa
+
+data =np.array(pd.read_csv('../meritve/data.csv', delimiter=','))
+
+time = data[:, 0] / 60
+meritve = unp.uarray(data[:, 1], 13 * [0.5]) * 1e-4
+
+meritve = 1 / (4 * np.pi * k ** 2) * (ploscina / meritve) ** 2
